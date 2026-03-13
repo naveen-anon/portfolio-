3036c6ad48697a552b86ba9c78f7ed8e4629974e
Naveen khatri <naveen1khatri@gmail.com>
2026-03-14 00:17:07 +0530

    Create globe.js

---
+++ globe.js
@@ -0,0 +1,17 @@
+const world = Globe()
+
+(document.getElementById("globe"))
+
+.globeImageUrl("//unpkg.com/three-globe/example/img/earth-dark.jpg")
+
+.pointsData([
+
+{lat:28.6,lng:77.2},
+{lat:40.7,lng:-74.0},
+{lat:35.6,lng:139.6}
+
+])
+
+.pointAltitude(0.1)
+
+.pointColor(()=> "red")
